cfa830d1c376fbc9281875be09c5834ddaa18c98
Sanghun1Adam1Park <sanghunpark0628@gmail.com>
2025-06-23 12:38:56 +0200

    Implemented functions/pre async

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,17 +1,36 @@
 import os
+import asyncio 
 
-def get_file_content(working_directory, file_path):
+def read_file(file: str) -> str:
+    """Read content of the file.
+
+    Args:
+        file (str): path to the file.
+
+    Returns:
+        str: contnet of the file in a format.
+    """
+    result = ""
+    with open(file, 'r') as f:
+        result += f.read(10000)
+        leftover = f.read()
+        if leftover:
+            result+=f'\n[...File "{file}" truncated at 10000 characters]'
     
-    expanded_base_path = os.path.expanduser("~/Projects/ai_agent/")
+    return result
+
+async def get_file_content(working_directory, file_path):
+    
+    expanded_base_path = await asyncio.to_thread(os.path.expanduser, "~/Projects/ai_agent/")
     working_directory = os.path.join(expanded_base_path, working_directory)
 
-    ls_working_dir = os.listdir(working_directory)
+    ls_working_dir = await asyncio.to_thread(os.listdir, working_directory)
     file_paths = file_path.split('/')
     if file_paths[0] not in ls_working_dir:
         return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
 
     if len(file_paths) > 1:
-        return get_file_content(
+        return await get_file_content(
             os.path.join(working_directory, file_paths[0]),
             '/'.join(file_paths[1:])
         )
@@ -19,13 +38,8 @@ def get_file_content(working_directory, file_path):
     if not os.path.isfile(file):
         return f'Error: File not found or is not a regular file: "{file}"'
 
-    result = ""
     try:
-        with open(file, 'r') as f:
-            result += f.read(10000)
-            leftover = f.read()
-            if leftover:
-                result+=f'\n[...File "{file}" truncated at 10000 characters]'
+        result = await asyncio.to_thread(read_file, file)
     except FileNotFoundError as e:
         return f'Error: File not found or is not a regular file: "{file}"'
 
--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,4 +1,5 @@
 import os 
+import asyncio
 
 def get_files_info(working_directory, directory=None):
     
